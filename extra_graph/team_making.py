0f61c8829d5a6e4bc969ba1827b734b0d3712cbf
Spooky-Scary-Skeleton <72188256+Spooky-Scary-Skeleton@users.noreply.github.com>
2020-10-29 22:29:15 +0900

    Create team_making.py

---
+++ extra_graph/team_making.py
@@ -0,0 +1,89 @@
+#---------------
+#[내풀이]
+#---------------
+
+#부모찾기
+def find_parent(parent,x):
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
+
+#합집합 수행
+def union(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+
+
+#학생정보,연산횟수 입력
+n, m = map(int, input().split())
+
+#부모 테이블 설정
+parent = [0] * (n+1)
+for i in range(n+1):
+  parent[i] = i
+
+operation = []
+for i in range(m):
+  operation.append(list(map(int, input().split())))
+
+for i in operation:
+  if i[0] == 0:
+    union(parent, i[1], i[2])
+  else:
+    if find_parent(parent, i[1]) == find_parent(parent, i[2]):
+      print("YES")
+    else:
+      print("NO")
+
+
+#---------------
+#[교재풀이]
+#---------------
+
+#부모찾기
+def find_parent(parent,x):
+  if parent[x] != x:
+    parent[x] = find_parent(parent, parent[x])
+  return parent[x]
+
+#합집합 수행
+def union(parent, a, b):
+  a = find_parent(parent, a)
+  b = find_parent(parent, b)
+  if a < b:
+    parent[b] = a
+  else:
+    parent[a] = b
+
+
+#학생정보,연산횟수 입력
+n, m = map(int, input().split())
+
+#부모 테이블 설정
+parent = [0] * (n+1)
+for i in range(n+1):
+  parent[i] = i
+
+
+for i in range(m):
+  oper, a, b = map(int, input().split())
+  if oper == 0:
+    union(parent, i[1], i[2])
+  else:
+    if find_parent(parent, i[1]) == find_parent(parent, i[2]):
+      print("YES")
+    else:
+      print("NO")
+      
+#--------------------
+#[부족한점]
+#--------------------
+oper, a, b로 바로 받고 반복문을 돌릴수 있게 코드를 더 깔끔하게 작성 가능했음. 
+
+
+
+

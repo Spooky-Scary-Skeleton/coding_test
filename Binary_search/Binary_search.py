17a122c47403151db0c76e9efa3d74b07fd8e761
Spooky-Scary-Skeleton <72188256+Spooky-Scary-Skeleton@users.noreply.github.com>
2021-03-14 14:20:30 +0900

    Update Binary_search.py

--- Binary_search/Binary_search.py
+++ Binary_search/Binary_search.py
@@ -26,3 +26,28 @@ else:
   print(result + 1)
 
 
+#---------------
+# 이진 탐색 소스코드 구현 (반복문)
+#---------------
+
+def binary_search(array, target, start, end):
+  while start <= end:
+    mid = (start + end) // 2
+    if array[mid] == target:
+      return target
+    elif target > array[mid]:
+      start = mid + 1
+    else:
+      end = mid -1
+
+#n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
+n, target = list(map(int, input().split()))
+#전체 원소 입력받기
+array = list(map(int, input().split()))
+
+#이진 탐색 수행 결과 출력
+result = binary_search(array, target, 0, n-1)
+if result == None:
+  print("원소가 존재하지 않습니다.")
+else:
+  print(result + 1)

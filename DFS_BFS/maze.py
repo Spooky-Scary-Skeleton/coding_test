79827ed0307bf27ee263751774d45ac3b55b5f4c
Spooky-Scary-Skeleton <72188256+Spooky-Scary-Skeleton@users.noreply.github.com>
2021-03-30 16:38:08 +0900

    Update maze.py

--- DFS_BFS/maze.py
+++ DFS_BFS/maze.py
@@ -12,6 +12,43 @@
 # ----------------------------------
 
 # ----------------------------------
+# #[내 풀이] 드디어 풀었음 210330
+# ----------------------------------
+from collections import deque
+
+n, m = map(int, input().split())
+
+#미로입력
+maze = []
+for i in range(n):
+  maze.append(list(map(int, input())))
+
+#0,0 부터 시작한다고 생각
+
+#상하좌우 움직임
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+
+#bFS
+def bfs(x, y):
+  q = deque()
+  q.append((x, y))
+  while q:
+    x, y = q.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if nx >= 0 and nx < n and ny >= 0 and ny < m and maze[nx][ny] == 1:
+        maze[nx][ny] += maze[x][y]
+        q.append((nx, ny))
+
+bfs(0, 0)
+
+print(maze[n-1][m-1])
+
+
+# ----------------------------------
 # #[교재풀이]
 # ----------------------------------
 

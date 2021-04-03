f22fcc18ec21197a1f0de8684e43f92bf0fde658
Spooky-Scary-Skeleton <72188256+Spooky-Scary-Skeleton@users.noreply.github.com>
2021-04-03 17:11:43 +0900

    Update currency.py

--- dynamic_programing/currency.py
+++ dynamic_programing/currency.py
@@ -7,6 +7,45 @@
 #------------------
 
 #------------------
+#[내풀이] 210403 어떻게 풀어내긴했음. 그러나 교재보다 지저분함. 
+#------------------
+
+#입력받기
+n, m = map(int, input().split())
+coin = []
+for i in range(n):
+  coin.append(int(input()))
+
+#불가능한 조합은 무한으로 표현
+max_num = int(1e9)
+
+#화폐별 조합가능 최소의 수
+combination = [max_num] * (m + 1)
+
+#특정동전 1종류만 썻을때의 최소조합의 수
+for i in coin:
+  k = 1
+  while i * k <= m:
+    combination[i * k] = min(k, combination[i * k]) 
+    k += 1
+
+#동전끼리 조합해서 만들었을때 최소조합의 수
+for i in range(1, m + 1):
+  for j in coin:
+    if i - j > 0 and combination[i-j] != max_num:
+      combination[i] = min(combination[i-j] + 1, combination[i])
+
+print(combination)
+
+
+if combination[m] != max_num:
+  print(combination[m])
+else:
+  print(-1)
+
+
+
+#------------------
 #[교재풀이] 사실 아직도 이런 발상을 어떻게 다이나믹이랑 연관지어서 떠올리는 건지 이해가 안됨. 
 #------------------
 
@@ -45,4 +84,10 @@ else:
 그리고 풀이내용을 봐도 아직 잘 이해가 안간다 ㅜㅜ;;;;
 """
   
- 
+#------------------
+#[부족한점] 210403
+#------------------
+"""
+풀어내긴 했지만 시간이 너무 오래걸렸고 교재보다는 더 지저분하다
+"""
+
